afeb83b78508df0d4e03566c957cf553a4d16db8
Mert Güney <mert_guney_29@hotmail.com>
2022-04-11 21:20:07 +0300

    Initial

---
+++ SignalRChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRChatApp.Data;
+using SignalRChatApp.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task GetUserId(string userId)
+        {
+            Client client = new Client()
+            {
+                ConnectionId = Context.ConnectionId,
+                UserId = userId
+            };
+            ClientSource.Clients.Add(client);
+            await Clients.Others.SendAsync("clientJoined", userId);
+        }
+
+        public async Task SendMessageAsync(string message, string userId)
+        {
+            Client client = ClientSource.Clients.FirstOrDefault(x => x.UserId == userId);
+
+            //await Clients.All.SendAsync("receiveMessage", message);//bütün clientlar
+            //await Clients.Others.SendAsync("receiveMessage", message);//Diğer clientlar
+
+            await Clients.Clients(client.ConnectionId).SendAsync("receiveMessage", message, client.UserId);//hedef clienttaki ilgili fonksiyon
+        }
+    }
+}
---
+++ SignalRChatApp/Models/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRChatApp.Models
+{
+    public class Client
+    {
+        public string ConnectionId { get; set; }
+        public string UserId { get; set; }
+    }
+}
